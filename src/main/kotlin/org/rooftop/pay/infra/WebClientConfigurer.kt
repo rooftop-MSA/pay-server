042b9962ce0c23fbf3d872924cf2c5165f1bd5b8
devxb <dlwnsdud205@naver.com>
2024-01-24 16:50:52 +0900

    feat: 서버통신 uri를 환경변수로 주입받도록 수정한다

--- src/main/kotlin/org/rooftop/pay/infra/WebClientConfigurer.kt
+++ src/main/kotlin/org/rooftop/pay/infra/WebClientConfigurer.kt
@@ -1,5 +1,6 @@
 package org.rooftop.pay.infra
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
@@ -7,11 +8,14 @@ import org.springframework.web.reactive.function.client.WebClient
 
 @Configuration
 @Profile("prod")
-class WebClientConfigurer {
+class WebClientConfigurer(
+    @Value("\${rooftop.server.identity:http://identity.rooftop.org}") private val identityServerUri: String,
+    @Value("\${rooftop.server.order:http://order.rooftop.org}") private val orderServerUri: String,
+) {
 
     @Bean
-    fun orderWebClient(): WebClient = WebClient.create("http://order.rooftop.org")
+    fun orderWebClient(): WebClient = WebClient.create(identityServerUri)
 
     @Bean
-    fun identityWebClient(): WebClient = WebClient.create("http://identity.rooftop.org")
+    fun identityWebClient(): WebClient = WebClient.create(orderServerUri)
 }
