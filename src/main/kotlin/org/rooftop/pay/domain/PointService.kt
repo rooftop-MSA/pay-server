5ce85be58b4877735b732584b9d40bb827cafc64
xb205 <62425964+devxb@users.noreply.github.com>
2024-01-31 14:51:16 +0900

    refactor: 낙관적 락 예외 발생시 retry 정책 수립 (#23)

--- src/main/kotlin/org/rooftop/pay/domain/PointService.kt
+++ src/main/kotlin/org/rooftop/pay/domain/PointService.kt
@@ -1,9 +1,13 @@
 package org.rooftop.pay.domain
 
 import org.springframework.context.event.EventListener
+import org.springframework.dao.OptimisticLockingFailureException
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import reactor.core.publisher.Mono
+import reactor.util.retry.RetrySpec
+import kotlin.time.Duration.Companion.milliseconds
+import kotlin.time.toJavaDuration
 
 @Service
 @Transactional(readOnly = true)
@@ -24,6 +28,7 @@ class PointService(
                 it.pay(price)
                 pointRepository.save(it)
             }
+            .retryWhen(retryOptimisticLockingFailure)
     }
 
     @Transactional
@@ -50,9 +55,16 @@ class PointService(
                 it.charge(pointRollbackEvent.paidPoint)
                 pointRepository.save(it)
             }
+            .retryWhen(retryOptimisticLockingFailure)
     }
 
     private companion object {
         private const val NEW_USER_BONUS_POINT = 1_000L
+        private const val RETRY_MOST_100_PERCENT = 1.0
+
+        private val retryOptimisticLockingFailure =
+            RetrySpec.fixedDelay(Long.MAX_VALUE, 50.milliseconds.toJavaDuration())
+                .jitter(RETRY_MOST_100_PERCENT)
+                .filter { it is OptimisticLockingFailureException }
     }
 }
